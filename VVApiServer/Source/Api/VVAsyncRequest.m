671657230fc6453409f707f6c7208fd734d03511
zhaojiangming <zhaojiangming@gmail.com>
2020-05-05 00:50:27 +0800

    优化字符串

--- VVApiServer/Source/Api/VVAsyncRequest.m
+++ VVApiServer/Source/Api/VVAsyncRequest.m
@@ -25,98 +25,22 @@
 
     self.sessionManager = [VVApiAFNFactory factory];
     self.sessionManager.completionQueue = completionQueue;
-    for (NSString *key in headers) {
-        [self.sessionManager.requestSerializer setValue:headers[key] forHTTPHeaderField:key];
-    }
+
     AFHTTPResponseSerializer <AFURLResponseSerialization> *responseSerializer = self.sessionManager.responseSerializer;
     responseSerializer.acceptableContentTypes = [responseSerializer.acceptableContentTypes setByAddingObjectsFromArray:@[@"text/html", @"text/plain"]];
 
-    if ([method isEqualToString:VV_API_HEAD]) {
-        [self headRequest:urlString params:params];
-    } else if ([method isEqualToString:VV_API_GET]) {
-        [self getRequest:urlString params:params];
-    } else if ([method isEqualToString:VV_API_POST]) {
-        [self postRequst:urlString params:params];
-    } else if ([method isEqualToString:VV_API_PUT]) {
-        [self putRequest:urlString params:params];
-    } else if ([method isEqualToString:VV_API_DELETE]) {
-        [self deleteRequuest:urlString params:params];
-    }
-}
-
-- (void)headRequest:(NSString *)urlString params:(NSDictionary *)params {
     VVWeak(self);
-    [self.sessionManager HEAD:urlString
-                   parameters:params
-                      success:^(NSURLSessionDataTask *task) {
-                          VVStrong(self);
-
-                          [self requsestSuccess:nil];
-                      } failure:^(NSURLSessionDataTask *task, NSError *error) {
+    [self.sessionManager dataTaskWithHTTPMethod:method
+                                      URLString:urlString
+                                     parameters:params
+                                        headers:headers
+                                 uploadProgress:NULL
+                               downloadProgress:NULL
+                                        success:^(NSURLSessionDataTask *task, id responseObject) {
+                                            VVStrong(self);
+                                            [self requsestSuccess:nil];
+                                        } failure:^(NSURLSessionDataTask *task, NSError *error) {
                 VVStrong(self);
-
-                [self requestFail:error response:task.response];
-            }];
-}
-
-- (void)getRequest:(NSString *)urlString params:(NSDictionary *)params {
-    VVWeak(self);
-    [self.sessionManager GET:urlString
-                  parameters:params
-                    progress:NULL
-                     success:^(NSURLSessionDataTask *task, id responseObject) {
-                         VVStrong(self);
-
-                         [self requsestSuccess:responseObject];
-                     } failure:^(NSURLSessionDataTask *task, NSError *error) {
-                VVStrong(self);
-
-                [self requestFail:error response:task.response];
-            }];
-}
-
-- (void)postRequst:(NSString *)urlString params:(NSDictionary *)params {
-    VVWeak(self);
-    [self.sessionManager POST:urlString
-                   parameters:params
-                     progress:NULL
-                      success:^(NSURLSessionDataTask *task, id responseObject) {
-                          VVStrong(self);
-
-                          [self requsestSuccess:responseObject];
-                      } failure:^(NSURLSessionDataTask *task, NSError *error) {
-                VVStrong(self);
-
-                [self requestFail:error response:task.response];
-            }];
-}
-
-- (void)putRequest:(NSString *)urlString params:(NSDictionary *)params {
-    VVWeak(self);
-    [self.sessionManager PUT:urlString
-                  parameters:params
-                     success:^(NSURLSessionDataTask *task, id responseObject) {
-                         VVStrong(self);
-
-                         [self requsestSuccess:responseObject];
-                     } failure:^(NSURLSessionDataTask *task, NSError *error) {
-                VVStrong(self);
-
-                [self requestFail:error response:task.response];
-            }];
-}
-
-- (void)deleteRequuest:(NSString *)urlString params:(NSDictionary *)params {
-    VVWeak(self);
-    [self.sessionManager DELETE:urlString
-                     parameters:params
-                        success:^(NSURLSessionDataTask *task, id responseObject) {
-                            VVStrong(self);
-
-                            [self requsestSuccess:responseObject];
-                        } failure:^(NSURLSessionDataTask *task, NSError *error) {
-                VVStrong(self);
-
                 [self requestFail:error response:task.response];
             }];
 }
